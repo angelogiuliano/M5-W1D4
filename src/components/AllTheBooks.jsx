4058656a28395b42178c0cf3735a46d629b4c8bd
ang <angelo03giuliano@gmail.com>
2024-02-14 09:40:02 +0100

    up input

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import { Book } from "./Book.jsx";
 import jsonData from "../books/horror.json";
+import { Placeholder } from "react-bootstrap";
 
 export function AllTheBooks() {
   let booksData = jsonData;
@@ -16,23 +17,28 @@ export function AllTheBooks() {
   };
 
   return (
-    <div className="row justify-content-center gap-5">
-      <input
-        type="text"
-        className=""
-        onChange={(e) => handleInput(e.target.value)}
-      />
-      {filteredBooks.map((book) => {
-        return (
-          <Book
-            key={book.asin}
-            imgSrc={book.img}
-            price={book.price}
-            title={book.title}
-            category={book.category}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="input-container w-100 d-flex justify-content-center">
+        <input
+          type="text"
+          className="w-50 m-4"
+          onChange={(e) => handleInput(e.target.value)}
+          placeholder="Search your book here!"
+        />
+      </div>
+      <div className="row justify-content-center gap-5">
+        {filteredBooks.map((book) => {
+          return (
+            <Book
+              key={book.asin}
+              imgSrc={book.img}
+              price={book.price}
+              title={book.title}
+              category={book.category}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
