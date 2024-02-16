f18f993c2ec65b5551bc6080ca352d7120a7f6e3
ang <angelo03giuliano@gmail.com>
2024-02-16 09:31:52 +0100

    up selected

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import './book.css';
 
 export const Book = ({ imgSrc, title, category, price }) => {
+
+  const [selected, setSelected] = useState(false)
+
   return (
     <Card className="px-0" style={{ width: "18rem" }}>
-      <Card.Img variant="img" src={imgSrc} />
+      <Card.Img onClick={() => setSelected(!selected)} className={`${selected && "selected"}`} variant="img" src={imgSrc} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>
